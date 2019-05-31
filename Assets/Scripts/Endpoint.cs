a3c960a7666bfaf24930c0559e95ec3b3a049ed1
Anirudh Ryali <aryali97@gmail.com>
2019-05-31 01:21:19 -0400

    Moved endpoint hoverability to own script

--- Assets/Scripts/Endpoint.cs
+++ Assets/Scripts/Endpoint.cs
@@ -6,24 +6,7 @@ using UnityEditor;
 
 public class Endpoint : IDable {
 
-    private Color hover_color_;
-    private Color orig_color_;
-    private Renderer renderer_;
-
     void Start() {
-        renderer_ = GetComponent<Renderer>();
-        hover_color_ = Color.magenta;
-        orig_color_ = renderer_.material.color;
-    }
-
-    // Mouse hovers over
-    void OnMouseEnter() {
-        renderer_.material.color = hover_color_;
-    }
-
-    // Mouse leaves hover
-    void OnMouseExit() {
-        renderer_.material.color = orig_color_;
     }
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Hoverable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoverable : MonoBehaviour {
+
+    private Color hoverColor;
+    private Color origColor;
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+	    renderer = GetComponent<Renderer>();
+        hoverColor = Color.magenta;
+        origColor = renderer.material.color;
+	}
+    
+    // Mouse hovers
+    void OnMouseEnter() {
+        renderer.material.color = hoverColor;
+    }
+
+    // Mouse leaves hover
+    void OnMouseExit() {
+        renderer.material.color = origColor;
+    }
+}
