756dc8a43324bc494cbe138b5feb249dd98d6d79
Anirudh Ryali <aryali97@gmail.com>
2019-06-06 15:58:46 -0400

    Frames update on player mouse up, add stage duplicates last frame and has right number, sets up initial frame on start

--- Assets/Scripts/AddStagePreview.cs
+++ Assets/Scripts/AddStagePreview.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,32 +9,32 @@ public class AddStagePreview : MonoBehaviour
     [SerializeField]
     private Button previewTemplate;
 
-    void Awake() {
-    }
+    private GameObject scrollContent;
 
-    public void OnButtonClick() {
-        var newButton = Instantiate(previewTemplate);
-        newButton.transform.SetParent(previewTemplate.transform.parent, false);
+    void Awake() {
+        scrollContent = GameObject.FindWithTag("Stage Preview Content");
 
         var balls = GameObject.FindGameObjectsWithTag("Player Ball");
-        Debug.Log("Balls length: " + balls.Length);
         foreach (var ball in balls) {
             var playerPrev = Instantiate(Resources.Load<Image>("Prefabs/Player Preview"));
-            playerPrev.transform.SetParent(newButton.transform);
-
+            playerPrev.transform.SetParent(scrollContent.transform.GetChild(0));
             var playerPrevRect = playerPrev.GetComponent<RectTransform>();
             playerPrevRect.localPosition = new Vector3(
                 ball.transform.position.x * 10,
                 ball.transform.position.z * 10,
                 0);
-            Debug.Log("Player pos: " + playerPrevRect.localPosition);
-            /*
-            playerPrev.transform.position = new Vector3(
-                ball.transform.position.x * 10,
-                ball.transform.position.z * 10,
-                0);
-            Debug.Log("Player pos: " + playerPrev.transform.position);
-            */
         }
+    }
+
+    public void OnButtonClick() {
+        // Change to selected from to duplicate
+        var frameToDuplicate = 
+            scrollContent.transform.GetChild(scrollContent.transform.childCount - 1)
+                .gameObject;
+        var newButton = Instantiate(frameToDuplicate);
+        newButton.transform.SetParent(scrollContent.transform, false);
+        newButton.transform.GetChild(1).GetComponent<Text>().text = "#" +
+            (Int32.Parse(frameToDuplicate.transform.GetChild(1)
+                 .GetComponent<Text>().text.Substring(1)) + 1);
     }
 }
---
+++ Assets/Scripts/PlayerUpdateFrame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUpdateFrame : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scrollContent;
+
+    void OnMouseUp() {
+        var lastFrame = scrollContent.transform.GetChild(
+            scrollContent.transform.childCount - 1);
+
+        // Delete all children
+        foreach (Transform child in lastFrame.transform) {
+            if (child.name.Contains("Player Preview")) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        // Add all players to frame
+        var balls = GameObject.FindGameObjectsWithTag("Player Ball");
+        foreach (var ball in balls) {
+            var playerPrev = Instantiate(Resources.Load<Image>("Prefabs/Player Preview"));
+            playerPrev.transform.SetParent(lastFrame);
+            var playerPrevRect = playerPrev.GetComponent<RectTransform>();
+            playerPrevRect.localPosition = new Vector3(
+                ball.transform.position.x * 10,
+                ball.transform.position.z * 10,
+                0); 
+        }
+    }
+}
