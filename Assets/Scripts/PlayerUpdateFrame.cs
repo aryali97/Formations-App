4b846e45a2e5278962ed126d5d68de71efe04302
Anirudh Ryali <aryali97@gmail.com>
2019-06-08 00:54:48 -0400

    Balls don't collide with each other

--- Assets/Scripts/PlayerUpdateFrame.cs
+++ Assets/Scripts/PlayerUpdateFrame.cs
@@ -5,6 +5,17 @@ using UnityEngine.UI;
 
 public class PlayerUpdateFrame : MonoBehaviour
 {
+    static int firstLayer = 8;
+    static int nextLayer = firstLayer;
+
+    void Start() {
+        gameObject.layer = nextLayer;
+        nextLayer++;
+        for (int i = firstLayer; i < nextLayer; i++) {
+            Physics.IgnoreLayerCollision(i, nextLayer);
+        }
+    }
+    
     void OnMouseUp() {
         FrameData.UpdateBallsInFrame(FrameData.selectedFrame);
     }
